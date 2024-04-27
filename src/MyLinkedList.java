0986c5f32197375d82fd762f74ccffcd087e0266
Batyr <bshekish@gmail.com>
2024-04-27 11:06:08 +0500

    second commit

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,26 +1,50 @@
-//public class MyLinkedList<E> implements MyList<E> {
-//
-//    private class MyNode{
-//        E data;
-//        MyNode next;
-//        MyNode prev;
-//
-//        MyNode(E data){
-//            this.data = data;
-//            this.next = null;
-//            this.prev = null;
-//        }
-//    }
-//
-//    private MyNode head;
-//    private MyNode tail;
-//    private int size;
-//
-//    public MyLinkedList(){
-//        head = null;
-//        tail = null;
-//        size = 0;
-//    }
-//
-//
-//}
+public class MyLinkedList<T> implements MyList<T> {
+
+    private class MyNode {
+        T data;
+        MyNode next;
+        MyNode prev;
+
+        MyNode(T data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    private MyNode head;
+    private MyNode tail;
+    private int size;
+
+    public MyLinkedList(){
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public void add(T item){
+        addLast(item);
+    }
+
+    public void set(int index, T item){
+        checkIndex(index);
+        MyNode current = getNode(index);
+        current.data = item;
+    }
+
+    public void addFirst(T item){
+        MyNode newNode = new MyNode(item);
+        if (isEmpty()){
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+    }
+
+
+
+}
